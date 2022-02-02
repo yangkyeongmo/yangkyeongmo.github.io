0b1d7ef2395fdbf3c87355cd41aed1ed245a816f
Nathan Gyeongmo Yang <nathan.yang@buzzvil.com>
2022-02-02 18:47:16 +0900

    feat(build): add build files

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Welcome to Jekyll!(Test)",
+        "excerpt":"Good day!  def welcome(visitor=you):   print(\"Good day,\", you, \"!\")  My github link: my-github   ","categories": ["test"],
+        "tags": [],
+        "url": "/test/first-post/",
+        "teaser": null
+      },{
+        "title": "암호화 해시 함수 의뢰 진행 내용: 암호화 해쉬 함수와 PyQt5",
+        "excerpt":"목차 1.암호화 해시 함수? 2.PyQt5의 사용 가이드 3.PyInstaller의 사용 방법 4.진행 내용 1.암호화 해시 함수? 암호화 해시 함수란, 위키피디아:암호화 해시 함수에 따르면: 암호화 해시 함수는 해시 함수의 일종으로, 해시값으로부터 원래의 입력값과의 관계를 찾기 어려운 성질을 가지는 경우를 의미한다. * 예를 들어, 해시 함수가 y=x같이 해시값(y)로부터 키값(x)를 알기 쉬운 경우, 암호화...","categories": ["dev"],
+        "tags": ["quest"],
+        "url": "/dev/hash-function-quest/",
+        "teaser": null
+      },{
+        "title": "PyInstaller를 사용하며 마주칠 수 있는 에러들",
+        "excerpt":"목차 1.Pyinstaller 2.가능한 에러들 PyInstaller PyInstaller? PyInstaller freezes (packages) Python applicaitions into stand-alone executables, under Windows, GNU/Linux, … PyInstaller는 Python 파일을 stand-alone 파일로 만들어줍니다. stand-alone이라고 하면, 외부 의존 없이 구동될 수 있다는 의미로, python(인터프리터)을 설치하지 않은 환경에서도 동작할 수 있습니다. 따라서 컴퓨터를 모르는 사람에게 (실행 가능한) 파이썬 코드를 전달해줄 때...","categories": ["dev"],
+        "tags": ["python"],
+        "url": "/dev/about-possible-errors-of-pyinstaller/",
+        "teaser": null
+      },{
+        "title": "전설로 떠나는 월가의 영웅 독후감",
+        "excerpt":"전설로 떠나는 투자의 영웅 독서 계기 최근 투자에 관심을 가지고 있다. 유행에 편승한 감도 없지 않지만 유튜브에서 본 얘기대로 일단 돈을 넣고 보니 남의 일이 되지 않아 공부를 해야겠다는 생각이 들었다. 관련해서 동영상을 찾아보다보면 피터 린치가 등장한다. 백발으로 나이는 있는것 같지만 눈에서 생기가 돌고 표정이 밝아 전혀 나이들어보이지 않는다. 한국의...","categories": ["book"],
+        "tags": ["finance"],
+        "url": "/book/book-report/",
+        "teaser": null
+      }]
